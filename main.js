22079a203a4e6189d072369429a972b245d48ca8
Madrine256 <madrinehrt1996@gmail.com>
2022-08-14 13:58:23 +0300

    styles and js classes

---
+++ main.js
@@ -0,0 +1,7 @@
+const closeBtn = document.querySelector('#corner-close-modal'),
+signup = document.querySelector('.signup'),
+submitBtn = document.querySelector('#submit'),
+cancelBtn = document.querySelector('#cancel'),
+overlayDiv = document.querySelector('.overlay'),
+modal_container = document.querySelector('.modal-container')
+
