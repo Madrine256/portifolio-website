b09f306c1004282dfe9a4c717faa20ca83514cc6
Madrine256 <madrinehrt1996@gmail.com>
2022-08-15 12:04:37 +0300

    web responsiveness

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-const closeBtn = document.querySelector('#corner-close-modal'),
-signup = document.querySelector('.signup'),
-submitBtn = document.querySelector('#submit'),
-cancelBtn = document.querySelector('#cancel'),
-overlayDiv = document.querySelector('.overlay'),
-modal_container = document.querySelector('.modal-container')
+const closeBtn = document.querySelector("#corner-close-modal"),
+  signup = document.querySelectorAll(".signup"),
+  submitBtn = document.querySelector("#submit"),
+  cancelBtn = document.querySelector("#cancel"),
+  overlayDiv = document.querySelector(".overlay"),
+  modal_container = document.querySelector(".modal-container");
 
+  signup.forEach(signupbtn =>{
+    signupbtn.addEventListener('click', (e)=>{
+        modal_container.style.display = 'block';
+
+        
+       
+    })
+  });
+
+  closeBtn.onclick = (e)=>{
+    //e.target.parentElement.remove();
+    modal_container.style.display = 'none';
+   }
+ 
+   cancelBtn.onclick = ()=>{
+     modal_container.style.display = 'none';
+  
+    }
